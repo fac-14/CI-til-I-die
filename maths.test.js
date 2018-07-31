08bc29c452a91ec95fbe1b69252fdd175352ebf2
Michael Watts <hello@michaelwatts.co>
2018-07-31 15:07:04 +0100

    set up travis

--- maths.test.js
+++ maths.test.js
@@ -14,3 +14,15 @@ tape("maths should have an add method", t => {
   t.equal(maths.add(10, 2), 12, "10+2 = 12");
   t.end();
 });
+
+tape("maths should have a subtract method", t => {
+  t.equal(
+    maths.hasOwnProperty("subtract"),
+    true,
+    "'subtract' key is present in object"
+  );
+  t.equal(typeof maths.add, "function", "maths.subtract is a function");
+  t.equal(maths.subtract(2, 1), 1, "2-1 = 1");
+  t.equal(maths.subtract(10, 2), 8, "10-2 = 8");
+  t.end();
+});
